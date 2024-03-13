6e2e5c12e37fd13357c27095553fb4c4c0f0220b
Diego Miranda <diegomo1224@gmail.com>
2024-03-13 15:38:41 -0300

    att

--- App.js
+++ App.js
@@ -9,15 +9,19 @@ import ImgComponent from "./src/Components/ImgComponent";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      {/* Componente pai 1 */}
       <BtnComponent>
+        {/* Componente filho 1 Log In */}
         <Txt1 />
       </BtnComponent>
-
+      {/* Componente pai 2 */}
       <Btn2Component>
+        {/* Componente filho 2 Sign In */}
         <Txt2 />
       </Btn2Component>
-
+      {/* Componente pai 3 */}
       <Btn3Component>
+        {/* Componente filho 3 imagem de exit  */}
         <ImgComponent />
       </Btn3Component>
     </View>
